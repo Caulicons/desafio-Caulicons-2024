d9574831b83fd7078f80921508bb6afd2d8e8245
Vitor Oliveira. <caulicons@gmail.com>
2024-10-12 07:33:34 -0300

    apply pure functions to methods for more Predictability and testability

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -3,27 +3,27 @@ import { bioma } from "./classes/biomas.js";
 import { Recinto } from "./classes/recintos.js";
 
 class RecintosZoo {
-    recintosViaveis = []
+    //Adicionando Especies disponíveis segundo o enunciado
+    especiesDisponiveis = {
+        LEAO: new Especie('LEAO', 3, true, [bioma.savana]),
+        LEOPARDO: new Especie('LEOPARDO', 2, true, [bioma.savana]),
+        CROCODILO: new Especie('CROCODILO', 3, true, [bioma.rio]),
+        MACACO: new Especie('MACACO', 1, false, [bioma.savana, bioma.floresta]),
+        GAZELA: new Especie('GAZELA', 2, false, [bioma.savana]),
+        HIPOPOTAMO: new Especie('HIPOPOTAMO', 4, false, [bioma.savana, bioma.rio]),
+    }
+
+    // Criando recintos do enunciado
+    recintosDisponiveis = [
+        new Recinto(1, [bioma.savana], 10),
+        new Recinto(2, [bioma.floresta], 5),
+        new Recinto(3, [bioma.savana, bioma.rio], 7),
+        new Recinto(4, [bioma.rio], 8),
+        new Recinto(5, [bioma.savana], 9),
+    ]
+
     constructor() {
-        //Adicionando Especies disponíveis segundo o enunciado
-        this.especiesDisponiveis = {
-            LEAO: new Especie('LEAO', 3, true, [bioma.savana]),
-            LEOPARDO: new Especie('LEOPARDO', 2, true, [bioma.savana]),
-            CROCODILO: new Especie('CROCODILO', 3, true, [bioma.rio]),
-            MACACO: new Especie('MACACO', 1, false, [bioma.savana, bioma.floresta]),
-            GAZELA: new Especie('GAZELA', 2, false, [bioma.savana]),
-            HIPOPOTAMO: new Especie('HIPOPOTAMO', 4, false, [bioma.savana, bioma.rio]),
-        }
-
-        // Criando recintos do enunciado
-        this.recintosDisponiveis = [
-            new Recinto(1, [bioma.savana], 10),
-            new Recinto(2, [bioma.floresta], 5),
-            new Recinto(3, [bioma.savana, bioma.rio], 7),
-            new Recinto(4, [bioma.rio], 8),
-            new Recinto(5, [bioma.savana], 9),
-        ]
-
+        this.recintosViaveis = this.recintosDisponiveis;
         //Adicionando animais do enunciado
         this.recintosDisponiveis[0].addAnimal(this.especiesDisponiveis.MACACO, 3)
         this.recintosDisponiveis[2].addAnimal(this.especiesDisponiveis.GAZELA, 1)
@@ -45,15 +45,15 @@ class RecintosZoo {
             }
 
         // Selecionando biomas adequados
-        this.selecionarBiomasAdequados(animal)
+        this.recintosViaveis = this.selecionarBiomasAdequados(animal, this.recintosViaveis)
         // Selecionando recintos com espaço livre
-        this.selecionarRecintosComEspacoLivre(animal, quantidade)
+        this.recintosViaveis = this.selecionarRecintosComEspacoLivre(animal, quantidade, this.recintosViaveis)
         // Lidando Com Animais Carnívoros
-        this.lidandoComAnimaisCarnivoros(animal)
+        this.recintosViaveis = this.lidandoComAnimaisCarnivoros(animal, this.recintosViaveis)
         // Verificando se o animal é hipopótamo, caso for, só pode ter outra especie caso o recinto for bioma savana e rio.
-        this.verificandoSerOAnimalEhHipopotamo(animal)
+        this.recintosViaveis = this.verificandoSerOAnimalEhHipopotamo(animal, this.recintosViaveis)
         //Um macaco não se sente confortável sem outro animal no recinto, seja da mesma ou outra espécie
-        this.verificandoSerOAnimalEhMamaco(animal, quantidade)
+        this.recintosViaveis = this.verificandoSerOAnimalEhMamaco(animal, quantidade, this.recintosViaveis)
 
         // Retorno resultado caso não tiver recintos viáveis
         if (this.recintosViaveis.length === 0)
@@ -73,23 +73,23 @@ class RecintosZoo {
         }
     }
 
-    selecionarBiomasAdequados(animal) {
-        this.recintosViaveis = this.recintosDisponiveis.filter(recinto =>
+    selecionarBiomasAdequados(animal, recintos) {
+        return recintos.filter(recinto =>
             recinto.biomas.some(bioma =>
                 animal.biomas.includes(bioma)
             )
         )
     }
 
-    selecionarRecintosComEspacoLivre(animal, quantidade) {
-        this.recintosViaveis = this.recintosViaveis.filter(recinto =>
+    selecionarRecintosComEspacoLivre(animal, quantidade, recintos) {
+        return recintos.filter(recinto =>
             recinto.espacoLivre() >= animal.tamanho * quantidade
         )
     }
 
-    lidandoComAnimaisCarnivoros(animal) {
+    lidandoComAnimaisCarnivoros(animal, recintos) {
         // Implementando a regra de que Animais carnívoros devem habitar somente com a própria espécie
-        this.recintosViaveis = this.recintosViaveis.filter(recinto => {
+        return recintos.filter(recinto => {
             // Se o recinto estiver vazio então não precisa verificar
             if (recinto.estaVazio()) return true
             // Verificando ser existe algum animal carnivoro 
@@ -100,23 +100,27 @@ class RecintosZoo {
         })
     }
 
-    verificandoSerOAnimalEhHipopotamo(animal) {
+    verificandoSerOAnimalEhHipopotamo(animal, recintos) {
 
-        if (animal.especie === this.especiesDisponiveis.HIPOPOTAMO.especie)
-            this.recintosViaveis = this.recintosViaveis.filter(recinto => {
-                const biomaEhSavanaRio = (recinto.biomas.includes(bioma.rio) && recinto.biomas.includes(bioma.savana))
-                return recinto.estaVazio() || biomaEhSavanaRio ? true
-                    : recinto.animais.some(animalNoRecinto =>
-                        animal.especie === animalNoRecinto.especie
-                    )
-            })
+        if (animal.especie !== this.especiesDisponiveis.HIPOPOTAMO.especie)
+            return recintos
+
+        return recintos.filter(recinto => {
+            const biomaEhSavanaRio = (recinto.biomas.includes(bioma.rio) && recinto.biomas.includes(bioma.savana))
+            return recinto.estaVazio() || biomaEhSavanaRio ? true
+                : recinto.animais.some(animalNoRecinto =>
+                    animal.especie === animalNoRecinto.especie
+                )
+        })
     }
 
-    verificandoSerOAnimalEhMamaco(animal, quantidade) {
-        if (animal.especie === this.especiesDisponiveis.MACACO.especie)
-            this.recintosViaveis = this.recintosViaveis.filter(recinto =>
-                recinto.quantidadeDeAnimais() > 0 || quantidade > 1 ? true : false
-            )
+    verificandoSerOAnimalEhMamaco(animal, quantidade, recintos) {
+        if (animal.especie !== this.especiesDisponiveis.MACACO.especie)
+            return recintos
+
+        return recintos.filter(recinto =>
+            recinto.quantidadeDeAnimais() > 0 || quantidade > 1 ? true : false
+        )
     }
 }
 
